0c145ef7354f9a8b5c72d711bc66ec3ebf5e5662
Akhrorbek Abdukhamitov <akhrrbk@gmail.com>
2022-01-07 16:25:27 +0500

    second commit

--- higherorderfunctions.js
+++ higherorderfunctions.js
@@ -8,6 +8,14 @@ var animals = [
     {name: 'rgw', animal: 'dog'}
 ]
 
+var orders = [
+    {amount: 123},
+    {amount: 270},
+    {amount: 623},
+    {amount: 562}
+]
+
+// filter 
 var dogs = animals.filter((axror) => axror.animal === 'dog')
 
 // var dogs = []
@@ -16,3 +24,26 @@ var dogs = animals.filter((axror) => axror.animal === 'dog')
 //         dogs.push(animals[i])
 //     }
 // }
+
+// map
+var dogsnames = animals.map((axror) => axror.name)
+
+// var dogsnames = []
+// for (let i = 0; i < animals.length; i++) {
+//     dogsnames.push(animals[i].name)
+// }
+
+
+// when you can't find the right list transformation
+// reduce
+var totalAmount = orders.reduce(function (sum, value) { 
+    // console.log(sum, value, value.amount);
+    return sum + value.amount
+}, 0)
+// zero is a starting value :)
+
+// var totalAmount = 0
+// for (let i = 0; i < orders.length; i++) {
+//     totalAmount += orders[i].amount    
+// }
+console.log(totalAmount);
