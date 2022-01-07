5ae85d418597b80699343fef6cda6cdedcbf2cdb
Akhrorbek Abdukhamitov <akhrrbk@gmail.com>
2022-01-07 16:01:24 +0500

    first commit

---
+++ higherorderfunctions.js
@@ -0,0 +1,18 @@
+var animals = [
+    {name: 'weurg', animal: 'dog'},
+    {name: 'grtgre', animal: 'cat'},
+    {name: 'gwrgrg', animal: 'rabbit'},
+    {name: 'erfgwrg', animal: 'rabbit'},
+    {name: 'grgre', animal: 'cat'},
+    {name: 'wgrreg', animal: 'dog'},
+    {name: 'rgw', animal: 'dog'}
+]
+
+var dogs = animals.filter((axror) => axror.animal === 'dog')
+
+// var dogs = []
+// for (let i = 0; i < animals.length; i++) {
+//     if(animals[i].animal === 'dog'){
+//         dogs.push(animals[i])
+//     }
+// }
